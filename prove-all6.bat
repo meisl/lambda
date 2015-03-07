ff231af5f40e6aca3cdb34c79fafd9f353c8c9d5
Matthias Kling <m.kling@sprax.eu>
2015-03-07 01:42:03 +0100

    add current P6 version to prove-all6.out

--- prove-all6.bat
+++ prove-all6.bat
@@ -1,8 +1,9 @@
 @echo off
+
 set WHERE=t\%1*.t t\%1**\*.t
 
 REM C:\Perl\bin\prove --state=failed,new,save --color --timer --failures --exec "perl6-m -Ilib" %WHERE%
-C:\Perl\bin\prove --color --timer --failures --exec "perl6-m -Ilib" %WHERE%
+perl6-m -v & C:\Perl\bin\prove --color --timer --failures --exec "perl6-m -Ilib" %WHERE%
 
 rem parrot-prove doesn't know --recurse - and...
 rem ...it simply hangs... :(
